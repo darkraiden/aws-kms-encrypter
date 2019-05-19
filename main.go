c89f7a5c6f9844fc0018bf3a88b89b045bb0be7c
Davide Di Mauro <dave@darkraiden.com>
2019-05-19 19:04:05 +0100

    add checkflags function

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
+	"errors"
 	"flag"
 )
 
 var usageMessage = "USAGE: ake -kms-id <yourKMSID> -context <encryptionContext>"
 
-// Flags defines the type of flags passed by the user to the application
-type Flags struct {
+// Flag defines the type of flags passed by the user to the application
+type Flag struct {
 	kmsID      *string
 	kmsContext *string
 }
 
 func main() {
 	// Get Flags
-	var flags Flags
+	var flags Flag
 	flags.kmsID = flag.String("kms-id", "", "the ID of your KMS key")
 	flags.kmsContext = flag.String("context", "", "the KMS encryption context")
 	flag.Parse()
@@ -24,7 +25,10 @@ func main() {
 	// Return Payload and raw password
 }
 
-func checkFlags(flags Flags) error {
+func checkFlags(flags Flag) error {
+	if *flags.kmsID == "" || *flags.kmsContext == "" {
+		return errors.New("Invalid flags.\nUsage: `aws-kms-encrypter -kms-id=\"ThisIsTheIDOfYourKMSKey\" -context=\"KMSEncryptionContext\"")
+	}
 
 	return nil
 }
